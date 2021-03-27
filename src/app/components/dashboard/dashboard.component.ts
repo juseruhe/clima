e8a23036de15370ef4ad4fd67f34fa6a2c214c3f
Sebastian <jsruiz7411@misena.edu.co>
2021-03-27 12:59:23 -0500

    Dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  urlImagen = 'https://previews.123rf.com/images/jemastock/jemastock1702/jemastock170211512/72744479-ilustraci%C3%B3n-de-vector-de-sol-tiempo-s%C3%ADmbolo-del-clima.jpg';
+  
+
   constructor() { }
 
   ngOnInit(): void {
